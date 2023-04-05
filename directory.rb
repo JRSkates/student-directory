63c79e76f6ca81478b74b8ba849e6c56c5faa850
JRSkates <skatesproduction@gmail.com>
2023-04-05 16:34:38 +0100

    Loading data from a file

--- directory.rb
+++ directory.rb
@@ -25,6 +25,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -36,6 +37,8 @@ def proccess(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
@@ -53,6 +56,17 @@ def save_students
     file.puts csv_line
   end
   file.close
+  puts "Students saved successfully"
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+  puts "Student.csv loaded and ready to view!"
 end
 
 def show_students
