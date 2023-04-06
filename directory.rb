b163a6e065ab130541575c380e2de1377e612d67
JRSkates <skatesproduction@gmail.com>
2023-04-06 16:09:59 +0100

    populate_student_array method defined (DRY)

--- directory.rb
+++ directory.rb
@@ -1,4 +1,9 @@
 @students = [] # an empty array accessible to all methods
+@cohort = :november
+
+def populate_student_array(name, cohort)
+  @students << {name: name, cohort: cohort.to_sym}
+end
 
 def input_students
   puts "Please enter the names of the students"
@@ -8,7 +13,8 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    @students << {name: name, cohort: :november}
+    populate_student_array(name, @cohort)
+    # @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     name = STDIN.gets.chomp
   end
@@ -63,7 +69,8 @@ def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
-    @students << {name: name, cohort: cohort.to_sym}
+    populate_student_array(name, cohort)
+    # @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
   puts "Student.csv loaded and ready to view!"
